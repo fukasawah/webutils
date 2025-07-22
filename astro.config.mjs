6f65b8a83c02e462dffb2854f9e86a4cce17aab1
fukasawah <fukasawah@live.jp>
2025-07-22 15:57:39 +0000

    feat: add JSON SQL Analyzer

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,16 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: process.argv.includes('--local') ? undefined : netlify()
+  adapter: process.argv.includes('--local') ? undefined : netlify(),
+  vite: {
+    optimizeDeps: {
+      exclude: ['@duckdb/duckdb-wasm']
+    },
+    server: {
+      headers: {
+        'Cross-Origin-Embedder-Policy': 'require-corp',
+        'Cross-Origin-Opener-Policy': 'same-origin'
+      }
+    }
+  }
 });
